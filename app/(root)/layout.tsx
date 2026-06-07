c18669bd9527fde771fb115c150c1c197eb28f17
Ashish Sharma <stackdev.ash@gmail.com>
2026-06-07 13:53:41 +0530

    feat: add onboarding gate to root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,9 @@
 import Header from "../../components/Header";
 import { auth } from "../../lib/nextauth/auth";
 import { redirect } from "next/navigation";
+import User from "@/database/models/user.model";
+import { connectToDatabase } from "@/database/mongoose";
+import OnboardingGate from "@/components/onboarding/OnboardingGate";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await auth();
@@ -14,9 +17,17 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
     email: session.user.email,
   };
 
+  await connectToDatabase();
+
+  const dbUser = await User.findOne({
+    email: session.user.email,
+  });
+
   return (
     <main className="min-h-screen text-gray-400">
       <Header user={user} />
+
+      <OnboardingGate show={!dbUser?.onboardingCompleted} userId={dbUser._id.toString()} />
 
       <div className="container py-10">{children}</div>
     </main>
