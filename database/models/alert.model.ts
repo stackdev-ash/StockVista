52cc71afcc922dbdd2a77cf7a54c056c3863e9ce
Ashish Sharma <stackdev.ash@gmail.com>
2026-06-11 16:25:03 +0530

    feat: create alert schema

---
+++ database/models/alert.model.ts
@@ -0,0 +1,64 @@
+import { Schema, model, models } from "mongoose";
+
+const AlertSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+
+    symbol: {
+      type: String,
+      required: true,
+    },
+
+    company: {
+      type: String,
+      required: true,
+    },
+
+    alertName: {
+      type: String,
+      required: true,
+    },
+
+    alertType: {
+      type: String,
+      enum: ["upper", "lower"],
+      required: true,
+    },
+
+    threshold: {
+      type: Number,
+      required: true,
+    },
+
+    currentPrice: {
+      type: Number,
+      default: 0,
+    },
+
+    changePercent: {
+      type: Number,
+      default: 0,
+    },
+
+    frequency: {
+      type: String,
+      enum: ["once", "hourly", "daily"],
+      default: "once",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    lastTriggeredAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default models.Alert || model("Alert", AlertSchema);
