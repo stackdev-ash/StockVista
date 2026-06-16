a8b7c4019e9ad28152d66f27bf0a1ad5ccc9b7f6
Ashish Sharma <stackdev.ash@gmail.com>
2026-06-16 11:23:06 +0530

    made stocks page

---
+++ app/(root)/stocks/[symbol]/page.tsx
@@ -0,0 +1,91 @@
+import TradingViewWidget from "../../../../components/TradingViewWidget";
+import WatchlistButton from "../../../../components/WatchlistButton";
+import { WatchlistItem } from "@/database/models/watchlist.model";
+import { getStocksDetails } from "@/lib/actions/finnhub.actions";
+import { getUserWatchlist } from "@/lib/actions/watchlist.actions";
+import {
+  SYMBOL_INFO_WIDGET_CONFIG,
+  CANDLE_CHART_WIDGET_CONFIG,
+  BASELINE_WIDGET_CONFIG,
+  TECHNICAL_ANALYSIS_WIDGET_CONFIG,
+  COMPANY_PROFILE_WIDGET_CONFIG,
+  COMPANY_FINANCIALS_WIDGET_CONFIG,
+} from "../../../../lib/constants";
+import { notFound, redirect } from "next/navigation";
+import { auth } from "@/lib/nextauth/auth";
+
+export default async function StockDetails({ params }: StockDetailsPageProps) {
+  const session = await auth();
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
+  const { symbol } = await params;
+  const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
+
+  const stockData = await getStocksDetails(symbol.toUpperCase());
+  const watchlist = await getUserWatchlist();
+
+  const isInWatchlist = watchlist.some(
+    (item: WatchlistItem) => item.symbol === symbol.toUpperCase(),
+  );
+
+  if (!stockData) notFound();
+
+  return (
+    <div className="flex min-h-screen p-4 md:p-4 lg:p-1">
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
+        {/* Left column */}
+        <div className="flex flex-col gap-6">
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}symbol-info.js`}
+            config={SYMBOL_INFO_WIDGET_CONFIG(symbol)}
+            height={170}
+          />
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}advanced-chart.js`}
+            config={CANDLE_CHART_WIDGET_CONFIG(symbol)}
+            className="custom-chart"
+            height={600}
+          />
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}advanced-chart.js`}
+            config={BASELINE_WIDGET_CONFIG(symbol)}
+            className="custom-chart"
+            height={600}
+          />
+        </div>
+
+        {/* Right column */}
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center justify-between">
+            <WatchlistButton
+              symbol={symbol.toUpperCase()}
+              company={stockData.company}
+              isInWatchlist={isInWatchlist}
+            />
+          </div>
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}technical-analysis.js`}
+            config={TECHNICAL_ANALYSIS_WIDGET_CONFIG(symbol)}
+            height={400}
+          />
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}company-profile.js`}
+            config={COMPANY_PROFILE_WIDGET_CONFIG(symbol)}
+            height={440}
+          />
+
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}financials.js`}
+            config={COMPANY_FINANCIALS_WIDGET_CONFIG(symbol)}
+            height={464}
+          />
+        </div>
+      </section>
+    </div>
+  );
+}
